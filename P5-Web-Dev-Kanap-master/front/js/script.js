6798f1327a11110f99385a21933ec2b5f18ccd22
Tom Mazzag <itstomazz@gmail.com>
2023-10-24 12:24:13 +0100

    Products added(API) with title, desc, and images

--- P5-Web-Dev-Kanap-master/front/js/script.js
+++ P5-Web-Dev-Kanap-master/front/js/script.js
@@ -1,11 +1,32 @@
+const items = document.getElementById('items');
+
+function newProduct(products, product) {
+    let newArticle = document.createElement('article');
+    let title = document.createElement('h3');
+    let description = document.createElement('p');
+    let image = document.createElement('img');
+
+    title.textContent = products[product].name;
+    title.classList.add('productName');
+    description.textContent = products[product].description;
+    description.classList.add('productDescription');
+    image.src = products[product].imageUrl;
+    image.alt = products[product].altTxt;
+
+    newArticle.appendChild(title);
+    newArticle.appendChild(description);
+    newArticle.appendChild(image);
+    return newArticle;
+}
+
 async function getText(file) {
     let myObject = await fetch(file);
     let products = await myObject.json();
     for (product in products) {
-        console.log(products[product].name)
+        items.appendChild(newProduct(products, product))
     }
-    let test = products[0]
-    console.log(test);
+
+    
 }
 
 getText('http://localhost:3000/api/products')
