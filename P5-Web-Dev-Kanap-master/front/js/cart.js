c3ddb7e9d6abb8cb75962d677acf168b56b51c74
Tom Mazzag <itstomazz@gmail.com>
2023-10-25 17:47:54 +0100

    Amount works properly, articles mostly done

--- P5-Web-Dev-Kanap-master/front/js/cart.js
+++ P5-Web-Dev-Kanap-master/front/js/cart.js
@@ -1,12 +1,12 @@
 const cart = document.getElementById('cart__items');
 let IDs = []
 let productIndexes = []
+let amount = 0;
 
 function getLocalStorage(products) {
     for(item in products) {
         if (localStorage.getItem(products[item].name) != null) {
             let result = JSON.parse(localStorage.getItem(products[item].name))
-            console.log(result)
             IDs.push(result)
         }
     }
@@ -18,24 +18,20 @@ function getLocalStorage(products) {
         } 
     }
 
-    Totals(products, productIndexes)
-
     for (indexes in productIndexes) {
         createArticle(products, productIndexes[indexes], IDs[indexes][0], IDs[indexes][1])
     }
+
+    Totals(products, productIndexes)
+
+    
 }
 
 function Totals (products, productIndexes) {
     let articlesTotal = document.getElementById('totalQuantity');
     let priceTotal = document.getElementById('totalPrice');
-    let amount = 0;
 
     articlesTotal.textContent = IDs.length;
-    for (prices in IDs) {
-        for (index in productIndexes) {
-            amount += IDs[prices][1] * products[index].price
-        }
-    }
     priceTotal.textContent = amount;
 }
 
@@ -57,25 +53,45 @@ function createArticle(products, productIndexes, coloursSelected, QuantitySelect
     let delText = document.createElement('p');
 
     newArticle.classList.add('cart__item');
+    newArticle.dataset.id = products[productIndexes]._id;
+    newArticle.dataset.color = coloursSelected;
     itemImgDiv.classList.add('cart__item__img');
     itemImg.src = products[productIndexes].imageUrl;
+    itemImg.alt = products[productIndexes].altTxt;
     itemContent.classList.add('cart__item__content');
     itemContentDesc.classList.add('cart__item__content__description');
     title.textContent = products[productIndexes].name;
     itemColour.textContent = coloursSelected;
     price.textContent = '€' + products[productIndexes].price * QuantitySelected;
+    contentSettings.classList.add('cart__item__content__settings')
+    contentSettingsQuantity.classList.add('cart__item__content__settings__quantity')
+    quantity.textContent = 'Quantity : ' + QuantitySelected;
+    changeQuantity.classList.add('itemQuantity')
+    changeQuantity.name = 'itemQuantity';
+    changeQuantity.type = 'number';
+    changeQuantity.value = QuantitySelected;
+    changeQuantity.min = 1;
+    changeQuantity.max = 100;
+    del.classList.add('cart__item__content__settings__delete');
+    delText.classList.add('deleteItem');
+    delText.textContent = 'Delete';
 
     newArticle.appendChild(itemImgDiv);
     itemImgDiv.appendChild(itemImg);
     newArticle.appendChild(itemContent);
-    itemContent.appendChild(itemContentDesc, contentSettings);
+    itemContent.appendChild(itemContentDesc);
+    itemContent.appendChild(contentSettings);
     itemContentDesc.appendChild(title);
     itemContentDesc.appendChild(itemColour)
     itemContentDesc.appendChild(price)
-    contentSettings.appendChild(contentSettingsQuantity, del)
-    contentSettingsQuantity.appendChild(quantity, changeQuantity)
+    contentSettings.appendChild(contentSettingsQuantity)
+    contentSettings.appendChild(del);
+    contentSettingsQuantity.appendChild(quantity)
+    contentSettingsQuantity.appendChild(changeQuantity);
     del.appendChild(delText);
     cart.appendChild(newArticle)
+
+    amount += products[productIndexes].price * QuantitySelected
 }
 
 async function addProducts(link) {
