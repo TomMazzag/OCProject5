c2c87ae4a07e1889dfe3bb04d3d9df324808d43d
Tom Mazzag <itstomazz@gmail.com>
2023-10-25 10:45:04 +0100

    Working on the cart

--- P5-Web-Dev-Kanap-master/front/js/product.js
+++ P5-Web-Dev-Kanap-master/front/js/product.js
@@ -2,10 +2,13 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id');
 
-const imageDiv = document.getElementById('image')
-const title = document.getElementById('title')
-const price = document.getElementById('price')
-const desc = document.getElementById('description')
+const imageDiv = document.getElementById('image');
+const title = document.getElementById('title');
+const price = document.getElementById('price');
+const desc = document.getElementById('description');
+const dropDown = document.getElementById('colors');
+
+const cart = document.getElementById('addToCart');
 
 function addDetails(product) {
     let image = document.createElement('img');
@@ -16,16 +19,39 @@ function addDetails(product) {
     title.textContent = product.name;
     price.textContent = product.price;
     desc.textContent = product.description;
+
+    for (colours in product.colors) {
+        let item = document.createElement('option');
+        item.textContent = product.colors[colours];
+        dropDown.appendChild(item)
+    }
 }
 
 async function addProduct(link) {
     let myObject = await fetch(link);
     let product = await myObject.json();
-    console.log(product)
     document.title = product.name;
     addDetails(product)
 }
 
 addProduct('http://localhost:3000/api/products/' + id)
 
+let selectedQuantity = '';
+let selectedColour = '';
+
+document.getElementById('quantity').addEventListener('change', $event => {
+    selectedQuantity = $event.target.value;
+})
+
+document.getElementById('colors').addEventListener('change', $event => {
+    selectedColour = $event.target.value;
+})
+
+cart.addEventListener('click', () => {
+    if (selectedQuantity > 0 && dropDown != '--Please, select a color --') {
+        added = [selectedColour, selectedQuantity, id]
+        localStorage.setItem('Product', added)
+    }
+    
+})
 
